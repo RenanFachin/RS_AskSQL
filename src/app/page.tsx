a7a57dbf24a29fac9f5064210631facbfac9bfc6
Renan <renan.fachin@hotmail.com>
2023-08-16 15:13:04 -0300

    feat: Applies a lib to create a style highlight in code entered by the user

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
+'use client'
+import { useState } from 'react';
 import Image from 'next/image'
 import logoImage from '../assets/logo.svg'
 
 import { Trash2, Stars } from 'lucide-react'
 
+// lib para syntax highlight
+import Editor from 'react-simple-code-editor';
+import { highlight, languages } from 'prismjs';
+import 'prismjs/components/prism-sql';
+import 'prismjs/themes/prism-funky.css'; //Example style, you can use another
+
+
 export default function Home() {
+  // funções do tipo useState são client side, precisa adicionar 'use client' ou criar um componente
+  const [code, setCode] = useState('')
+  const [question, setQuestion] = useState('')
+
+  const [result, setResult] = useState('')
+
   return (
     <div className='max-w-[430px] mx-auto px-4 pt-12 pb-4'>
       {/* Logo + delete button */}
@@ -23,10 +38,19 @@ export default function Home() {
         >
           Cole seu código SQL aqui:
         </label>
-        <textarea
-          className='my-4 bg-blueberry-600 border border-blueberry-300 rounded-md px-4 py-3 outline-none focus:ring-2 focus:ring-pistachio' name="schema"
-          id="schema"
+
+        <Editor
+          textareaId='schema'
+          value={code}
+          onValueChange={code => setCode(code)}
+          highlight={code => highlight(code, languages.sql, 'sql')}
+          // Não estava aceitando o padding no className com tailwindcss
+          padding={16}
+          textareaClassName='outline-none'
+          // focus-within pq o elemento vai apresentar o focus quando o elemento pai for focado tbm
+          className='my-4 font-mono h-40 bg-blueberry-600 border border-blueberry-300 rounded-md focus-within:ring-2 focus-within:ring-pistachio'
         />
+
 
         <label
           htmlFor="question"
@@ -35,9 +59,13 @@ export default function Home() {
           Faça uma pergunta sobre o código:
         </label>
         <textarea
-          className='my-4 bg-blueberry-600 border border-blueberry-300 rounded-md px-4 py-3 outline-none focus:ring-2 focus:ring-pistachio' name="question"
+          className='my-4 bg-blueberry-600 border border-blueberry-300 rounded-md px-4 py-3 outline-none focus:ring-2 focus:ring-pistachio'
+          name="question"
           id="question"
+          value={question}
+          onChange={e => setQuestion(e.target.value)}
         />
+
 
         <button
           type="submit"
@@ -51,9 +79,18 @@ export default function Home() {
       {/* resposta da ai */}
       <div className='mt-6'>
         <span className='text-lg font-light text-snow'>Sua resposta:</span>
-        <textarea
+
+        <Editor
+          textareaId='schema'
           readOnly
-          className='w-full my-4 bg-transparent border border-blueberry-300 rounded-md px-4 py-3 outline-none focus:ring-2 focus:ring-lime-700'
+          value={result}
+          onValueChange={() => {}} // Como é um parâmetro obrigatório, estamos deixando uma função sem "nada"
+          highlight={code => highlight(code, languages.sql, 'sql')}
+          // Não estava aceitando o padding no className com tailwindcss
+          padding={16}
+          textareaClassName='outline-none'
+          // focus-within pq o elemento vai apresentar o focus quando o elemento pai for focado tbm
+          className='w-full my-4 bg-transparent border border-blueberry-300 rounded-md'
         />
       </div>
     </div>
