f32d51ea65a96b2d3aa8a219524da2b5fdfa39a0
Renan <renan.fachin@hotmail.com>
2023-08-16 13:55:08 -0300

    import logo and fonts using nextFont

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,10 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      sans: ['var(--font-inter)'],
+      mono: ['var(--font-ubuntu)']
+    },
   },
   plugins: [],
 }
